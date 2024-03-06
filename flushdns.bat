d319bc383df9e855d91daa2390f17f381e305971
factxd01 <162409476+factxd01@users.noreply.github.com>
2024-03-06 12:42:41 +0700

    Add files via upload

---
+++ flushdns.bat
@@ -0,0 +1,20 @@
+
+@echo off
+netsh int ip set address "%%j" dhcp 
+netsh int ip set dns "%%j" dhcp 
+netsh interface set interface name="%%j" admin=enabled 
+certutil -URLCache * delete 
+netsh int ip reset 
+netsh int ipv4 reset 
+netsh int ipv6 reset 
+ipconfig / >nul
+ipconfig /release >nul
+ipconfig /renew >nul
+ipconfig /flushdns >nul
+netsh advfirewall reset
+netsh winsock reset
+netsh int ip reset
+echo 100% 
+netstat -e
+timeout 1
+echo Successfully : Press Any Key To Continue)
